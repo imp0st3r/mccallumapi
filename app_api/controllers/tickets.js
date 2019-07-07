0aab020cf506e0ebb80fae47a02eb2a43b58c668
Luke Harris <luke.harris@student.centralia.edu>
2019-07-07 09:30:37 -0700

    changed update tickets

--- app_api/controllers/tickets.js
+++ app_api/controllers/tickets.js
@@ -83,87 +83,16 @@ module.exports.updateTicket = function(req, res) {
 	if(!req.params.id){
 		sendJsonResponse(res, 400, {"message": "Not found, ticket id is required"});
 		return;
-	}
-	Ticket.findById(req.params.id).exec(function(err, ticket) {
-        if(!ticket) {
-            sendJsonResponse(res, 400, {"message" : "ticket not found"});
-            return;
-        } else if (err) {
-            sendJsonResponse(res, 400, err);
-            return;
+    }
+    var ticket = req.body;
+    Ticket.findOneAndUpdate({_id:req.params.id},ticket,{new:true},function(err,doc){
+        if(err){
+            console.log(err);
+            sendJsonResponse(res,400,err);
         }else{
-            if(req.body.creator){
-                ticket.creator = req.body.creator;
-            }
-            if(req.body.worker){
-                ticket.worker = req.body.worker;
-            }
-            if(req.body.transaction_date){
-                ticket.transaction_date = req.body.transaction_date;
-            }
-            if(req.body.reference_number){
-                ticket.reference_number = req.body.reference_number;
-            }
-            if(req.body.customer_name){
-                ticket.customer_name = req.body.customer_name;
-            }
-            if(req.body.job_number){
-                ticket.job_number = req.body.job_number;
-            }
-            if(req.body.supplier){
-                ticket.supplier = req.body.supplier;
-            }
-            if(req.body.operator){
-                ticket.operator = req.body.operator;
-            }
-            if(req.body.items){
-                ticket.items = req.body.items;
-            }
-            if(req.body.hazmat){
-                ticket.hazmat = req.body.hazmat;
-            }
-            if(req.body.status){
-                ticket.status = req.body.status;
-            }else{
-                ticket.status = "open";
-            }
-            if(req.body.trip_route){
-                ticket.trip_route = req.body.trip_route;
-            }
-            if(req.body.return_route){
-                ticket.return_route = req.body.return_route;
-            }
-            if(req.body.driver){
-                ticket.driver = req.body.driver;
-            }
-            if(req.body.truck_number){
-                ticket.truck_number = req.body.truck_number;
-            }
-            if(req.body.hazmat_shipping_confirmation){
-                ticket.hazmat_shipping_confirmation = req.body.hazmat_shipping_confirmation;
-            }else{
-                ticket.hazmat_shipping_confirmation = false;
-            }
-            if(req.body.hazmat_return_confirmation){
-                ticket.hazmat_return_confirmation = req.body.hazmat_return_confirmation;
-            }else{
-                ticket.hazmat_return_confirmation = false;
-            }
-            if(req.body.hazmat_return_paper){
-                ticket.hazmat_return_paper = req.body.hazmat_return_paper;
-            }
-            if(req.body.hazmat_shipping_paper){
-                ticket.hazmat_shipping_paper = req.body.hazmat_shipping_paper;
-            }
-            ticket.save(function(err, nticket){
-                if (err) {
-                    sendJsonResponse(res, 400, err);
-                } else {
-                    sendJsonResponse(res, 200, nticket);
-                }
-            });
-        }   
-    });
+            sendJsonResponse(res,200,doc);
+        }
+    })
 };
 module.exports.deleteTicket = function(req, res) {
 	if (req.params.id) {
